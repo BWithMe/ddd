ad3b89257ac15245199b9982d576fcd317676ac1
odinxcode@gmail.com <odinxcode@gmail.com>
2022-02-06 13:21:18 +0000

    File generated by Skaffolder - user: odinxcode@gmail.com

---
+++ properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_ddd_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_ddd_db_User = 'root';
+	$db_ddd_db_Pass = '';
+	$db_ddd_db_DbName = 'ddd_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
